a6361680d55b0df6179a5cc025cda7f2647df219
DESKTOP-4MAPJEF\鳳梨 <max3026783@gmail.com>
2023-07-11 01:38:29 +0800

    add to db

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
+using BuildingInfoAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<BuildingContext>(context => context.UseInMemoryDatabase("BuildingList"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
