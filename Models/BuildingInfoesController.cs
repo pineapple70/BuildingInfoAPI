815da62b0b74b78dc5882399ef20a28f5da2c7cf
DESKTOP-4MAPJEF\鳳梨 <max3026783@gmail.com>
2023-07-11 04:42:56 +0800

    改串接LocalDB

---
+++ Models/BuildingInfoesController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BuildingInfoAPI.Models
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BuildingInfoesController : ControllerBase
+    {
+        private readonly BuildingContext _context;
+
+        public BuildingInfoesController(BuildingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/BuildingInfoes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BuildingInfo>>> GetBuildingInfos()
+        {
+          if (_context.BuildingInfo == null)
+          {
+              return NotFound();
+          }
+            return await _context.BuildingInfo.ToListAsync();
+        }
+
+        // GET: api/BuildingInfoes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BuildingInfo>> GetBuildingInfo(long id)
+        {
+          if (_context.BuildingInfo == null)
+          {
+              return NotFound();
+          }
+            var buildingInfo = await _context.BuildingInfo.FindAsync(id);
+
+            if (buildingInfo == null)
+            {
+                return NotFound();
+            }
+
+            return buildingInfo;
+        }
+
+        // PUT: api/BuildingInfoes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBuildingInfo(long id, BuildingInfo buildingInfo)
+        {
+            if (id != buildingInfo.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(buildingInfo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BuildingInfoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/BuildingInfoes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<BuildingInfo>> PostBuildingInfo(BuildingInfo buildingInfo)
+        {
+          if (_context.BuildingInfo == null)
+          {
+              return Problem("Entity set 'BuildingContext.BuildingInfos'  is null.");
+          }
+            _context.BuildingInfo.Add(buildingInfo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBuildingInfo", new { id = buildingInfo.Id }, buildingInfo);
+        }
+
+        // DELETE: api/BuildingInfoes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBuildingInfo(long id)
+        {
+            if (_context.BuildingInfo == null)
+            {
+                return NotFound();
+            }
+            var buildingInfo = await _context.BuildingInfo.FindAsync(id);
+            if (buildingInfo == null)
+            {
+                return NotFound();
+            }
+
+            _context.BuildingInfo.Remove(buildingInfo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BuildingInfoExists(long id)
+        {
+            return (_context.BuildingInfo?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<BuildingContext>(context => context.UseInMemoryDatabase("BuildingList"));
+builder.Services.AddDbContext<BuildingContext>(context =>
+context.UseSqlServer("Data Source=(localdb)\\ProjectModels;Initial Catalog=model;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
